70344c13c136d3a719941cd980b7421f5d7c3f2b
Nabil ML <nabil.oumellahni01@gmail.com>
2023-09-05 19:38:57 +0100

    Microservices communication using WebClient

--- employee-service/src/main/java/com/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -1,5 +1,7 @@
 package com.employeeservice.service.impl;
 
+import com.employeeservice.dto.ApiResponseDto;
+import com.employeeservice.dto.DepartmentDto;
 import com.employeeservice.dto.EmployeeDto;
 import com.employeeservice.entity.Employee;
 import com.employeeservice.mapper.EmployeeMapper;
@@ -7,6 +9,7 @@ import com.employeeservice.repository.EmployeeRepository;
 import com.employeeservice.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,6 +20,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
     private EmployeeMapper employeeMapper;
+    private WebClient webClient;
 
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
@@ -25,9 +29,27 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
-    public EmployeeDto getEmployeeById(Long id) {
+    public ApiResponseDto getEmployeeById(Long id) {
         Employee employee = employeeRepository.findById(id).get();
-        return employeeMapper.mapToDto(employee);
+
+        // Create an object to store the HTTP response
+        DepartmentDto departmentDto = webClient.get()
+                // URI for the HTTP request
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                // Trigger the HTTP request and retrieve the response
+                .retrieve()
+                // Treat the response body as a DepartmentDto object
+                .bodyToMono(DepartmentDto.class)
+                // Block the current thread while waiting for the response
+                .block();
+
+        EmployeeDto employeeDto = employeeMapper.mapToDto(employee);
+
+        ApiResponseDto apiResponseDto = new ApiResponseDto();
+        apiResponseDto.setEmployee(employeeDto);
+        apiResponseDto.setDepartment(departmentDto);
+
+        return apiResponseDto;
     }
 
     @Override
